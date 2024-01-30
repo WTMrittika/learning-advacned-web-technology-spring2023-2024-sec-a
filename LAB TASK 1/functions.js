a1b1e3afc19588dbd6753dbd4356a7a241f1e900
WTMrittika <21-45108-2@student.aiub.edu>
2024-01-30 13:57:13 +0600

    task 1

---
+++ LAB TASK 1/functions.js
@@ -0,0 +1,21 @@
+function greet(name){
+    console.log(`Hello, ${name}!`);
+}
+greet ("Alice");
+
+//function return
+function sum(a, b){
+    return a+b;
+}
+let result = sum(1, 2);
+console.log(result);
+
+//Arrow function
+let hello = name => console.log(`Hello, ${name}!`);
+hello("Mrittika");
+
+//Anonymous function
+const hii = function(name){
+    console.log(`Hello, ${name}!`);
+};
+hii("Rayhan");
