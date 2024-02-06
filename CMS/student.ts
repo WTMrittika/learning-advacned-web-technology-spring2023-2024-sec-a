dc83beb69f56537560fe70638b363f7a5ebc546a
WTMrittika <21-45108-2@student.aiub.edu>
2024-02-06 13:55:09 +0600

    course management system

---
+++ CMS/student.ts
@@ -0,0 +1,12 @@
+class student{
+    id:number;
+    name:string;
+    cgpa:number;
+    constructor(id:number, name:string, cgpa:number){
+        this.id=id;
+        this.name=name;
+        this.cgpa=cgpa
+    }
+
+
+}
